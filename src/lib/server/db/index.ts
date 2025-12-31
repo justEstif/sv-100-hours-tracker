901fbb927bd2c770fc332ed797fe8d28c9ebea6d
estifanos <95101485+justEstif@users.noreply.github.com>
2025-12-31 18:39:29 -0500

    fix: use dummy value when building for the postgres connection

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,10 +1,13 @@
+import { building } from "$app/environment";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./schema";
 import { env } from "$env/dynamic/private";
 
-if (!env.DATABASE_URL) throw new Error("DATABASE_URL is not set");
+const DATABASE_URL = building ? "postgres://build:build@localhost:5432/build" : env.DATABASE_URL;
 
-const client = postgres(env.DATABASE_URL);
+if (!DATABASE_URL) throw new Error("DATABASE_URL is not set");
+
+const client = postgres(DATABASE_URL);
 
 export const db = drizzle(client, { schema });
